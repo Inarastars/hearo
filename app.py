f31830db2e597cca705176b25e72e528bbf8863d
silvermete0r <gregormelon.2003@gmail.com>
2025-04-28 13:29:50 +0500

    upd-deploy-500

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, jsonify, request
 import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -36,4 +40,5 @@ def get_categories():
     return jsonify(sorted(categories))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=(os.environ.get('FLASK_ENV') == 'development'))
